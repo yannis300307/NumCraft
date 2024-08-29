7554168c4da295e9be7342a4665d74858585e5d3
yannis300307 <yannis300307@gmail.com>
2024-08-29 13:45:33 +0200

    Rename macro

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -1,6 +1,6 @@
 #include "chunk.hpp"
 
-#define get_index(x, y, z) x + y *CHUNK_SIZE + z *CHUNK_SIZE *CHUNK_SIZE
+#define get_local_index(x, y, z) x + y *CHUNK_SIZE + z *CHUNK_SIZE *CHUNK_SIZE
 
 Chunk::Chunk(unsigned int x, unsigned int y, unsigned int z)
 {
@@ -18,10 +18,10 @@ bool Chunk::set_block(uint32_t x, uint32_t y, uint32_t z, block_t type)
 {
     if ((x >= CHUNK_SIZE) || (y >= CHUNK_SIZE) || (z >= CHUNK_SIZE))
         return false;
-    blocks[get_index(x, y, z)] = type;
+    blocks[get_local_index(x, y, z)] = type;
     return true;
 }
 
 block_t Chunk::get_block(uint32_t x, uint32_t y, uint32_t z) {
-    return blocks[get_index(x, y, z)];
+    return blocks[get_local_index(x, y, z)];
 }
