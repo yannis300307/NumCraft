af544976f644e995665e9863c1e321976583789b
Yannis300307 <yannis300307@gmail.com>
2024-10-06 10:33:57 +0200

    Finished list implementation

--- src/list.hpp
+++ src/list.hpp
@@ -4,26 +4,44 @@
     struct t##_list_cell                                                      \
     {                                                                         \
         t value;                                                              \
-        t##_list_cell *next = nullptr;                                        \
+        t##_list_cell *next = NULL;                                           \
     };                                                                        \
     struct t##_list                                                           \
     {                                                                         \
-        t##_list_cell *last = nullptr;                                        \
-        t##_list_cell *first = nullptr;                                       \
+        t##_list_cell *last = NULL;                                           \
+        t##_list_cell *first = NULL;                                          \
         size_t size = 0;                                                      \
     };                                                                        \
     bool push_back(t##_list *list, t value)                                   \
     {                                                                         \
         t##_list_cell *cell = (t##_list_cell *)malloc(sizeof(t##_list_cell)); \
-        if (list->size == 0)                                                  \
-            list->first = cell;                                               \
+        if (cell == NULL)                                                     \
+            return false;                                                     \
         cell->value = value;                                                  \
-        list->last->next = cell;                                              \
+        if (list->size == 0)                                                  \
+        {                                                                     \
+            list->first = cell;                                               \
+        }                                                                     \
+        else                                                                  \
+        {                                                                     \
+            list->last->next = cell;                                          \
+        }                                                                     \
         list->last = cell;                                                    \
+        list->size += 1;                                                      \
+        return true;                                                          \
     }                                                                         \
-    t get_item(int index)                                                     \
+    struct t##_list_iterator                                                  \
     {                                                                         \
-        /*TODO*/                                                              \
+        t##_list *list;                                                       \
+        int cursor = 0;                                                       \
+        t##_list_cell *current = list->first;                                 \
+    };                                                                        \
+    t *iterator_next(t##_list_iterator *iterator)                             \
+    {                                                                         \
+        if (iterator->cursor >= iterator->list->size)                         \
+            return NULL;                                                      \
+        t##_list_cell *current = iterator->current;                           \
+        iterator->current = iterator->current->next;                          \
+        iterator->cursor += 1;                                                \
+        return &(current->value);                                             \
     }
-
-LIST_CELL(int);
